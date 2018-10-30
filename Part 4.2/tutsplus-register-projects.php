e827443651d6f169451b7aadef43f6328151a0e1
Adam Brown <adamarthurryan@gmail.com>
2018-10-29 21:21:51 -0400

    project checkin

---
+++ Part 4.2/tutsplus-register-projects.php
@@ -0,0 +1,95 @@
+<?php
+	/**
+	Plugin Name: Tutsplus plugin to register post type
+	Plugin URI: https://code.tutsplus.com
+	Description: Registers the 'project' post type
+	Version: 4.2
+	Author: Rachel McCollin
+	Author URI: https://rachelmccollin.com
+	*/
+
+/******************************************************************
+function tutsplus_register_post_type() - registers our post type
+*******************************************************************/
+function tutsplus_register_post_type() {
+	
+	$labels = array(
+		'name' => __( 'Projects', 'tutsplus' ),
+		'singular_name' => __( 'Project', 'tutsplus' ),
+		'add_new' => __( 'Add New Project', 'tutsplus' ),
+		'add_new_item' => __( 'Add New Project', 'tutsplus' ),
+		'edit_item' => __( 'Edit Project', 'tutsplus' ),
+		'new_item' => __( 'New Project', 'tutsplus' ),
+		'view_item' => __( 'View Project', 'tutsplus' ),
+		'search_items' => __( 'Search Projects', 'tutsplus' ),
+		'not_found' => __( 'No Projects Found', 'tutsplus' ),
+		'not_found_in_trash' => __( 'No Projects Found in Trash', 'tutsplus' ),
+	);
+	
+	$args = array(
+		'labels' => $labels,
+		'has_archive' =>true,
+		'public' => true,
+		'hierarchical' => false,
+		'rewrite' => array( 'slug' => 'projects' ),
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'custom-fields',
+			'thumbnail',
+			'page-attributes'
+		),
+		'taxonomies' => array( 'post_tag', 'category' )
+	);
+	
+	register_post_type( 'tutsplus_project', $args );
+	
+	
+}
+add_action( 'init', 'tutsplus_register_post_type' );
+
+/****************************************************************************
+Register custom taxonomy
+****************************************************************************/
+function tutsplus_register_taxonomy() {	
+	
+	// genres
+	$labels = array(
+		'name'              => __( 'Project Types' ),
+		'singular_name'     => __( 'Project Type' ),
+		'search_items'      => __( 'Search Project Types' ),
+		'all_items'         => __( 'All Project Types' ),
+		'edit_item'         => __( 'Edit Project Types' ),
+		'update_item'       => __( 'Update Project Types' ),
+		'add_new_item'      => __( 'Add New Project Types' ),
+		'new_item_name'     => __( 'New Project Type Name' ),
+		'menu_name'         => __( 'Project Types' ),
+	);
+	
+	$args = array(
+		'labels' => $labels,
+		'hierarchical' => true,
+		'sort' => true,
+		'args' => array( 'orderby' => 'term_order' ),
+		'rewrite' => array( 'slug' => 'project-type' ),
+		'show_admin_column' => true
+	);
+	
+	register_taxonomy( 'tutsplus_project_type', array( 'tutsplus_project' ), $args);
+		
+}
+add_action( 'init', 'tutsplus_register_taxonomy' );
+
+/****************************************************************************
+Modify loop on projects archive page
+****************************************************************************/
+function tutsplus_modify_loop_projects( $query ) {
+	
+	if ( $query->is_post_type_archive( 'tutsplus_project') && $query->is_main_query() ) {
+		$query->set( 'orderby', 'title' );
+		$query->set( 'order', 'ASC' );
+	}
+	
+}
+add_action( 'pre_get_posts', 'tutsplus_modify_loop_projects' );
---
+++ README.md
@@ -0,0 +1,22 @@
+# [Guide to The Loop in WordPress][published url]
+## Instructor: [Rachel McCollin][instructor url]
+
+
+The Loop is at the core of WordPress's post rendering engine. With The Loop, WordPress formats each post to be displayed on the current page. If you want to customize the way posts are displayed in your WordPress theme, you'll need to come to terms with The Loop.
+
+In this course you'll learn how The Loop works in WordPress. You'll learn how to code your own loop and how to modify the loop in your themes and plugins. You'll also see how to create a loop from scratch for the ultimate in customizability.
+
+
+## Source Files Description
+
+
+Source files relate to each part of the course where code is created or edited, with version numbers assigned according to the course part. There is also a final version of the theme.
+
+------
+
+These are source files for the Envato Tuts+ course: [Guide to The Loop in WordPress][published url]
+
+Available on [Tuts+](https://tutsplus.com). Teaching skills to millions worldwide.
+
+[published url]: https://code.tutsplus.com/courses/guide-to-the-loop-in-wordpress
+[instructor url]: https://tutsplus.com/authors/rachel-mccollin
